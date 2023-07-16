30d4c04a8a7eb47cfd0c4f6e58052a4dd90ea902
Subodh <subodhmaharjan33@gmail.com>
2023-07-17 08:42:37 +1000

    everything

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -21,6 +21,7 @@ const Home: NextPage = () => {
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-8">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> Turbo
+            MOTHER FUCKER
           </h1>
           <AuthShowcase />
 
